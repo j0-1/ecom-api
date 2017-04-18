bc69c7a64cf09ec18cf5493f3da98f0fb355be86
Joe Straitiff <jstraitiff@gmail.com>
2017-04-17 19:30:14 -0700

    Add active_model_serializers
    Serialize objects using active_model_serializers and optin to the
    breaking change

---
+++ config/initializers/active_model_serializers.rb
@@ -0,0 +1,3 @@
+# Opting in to breaking change per
+# https://github.com/rails-api/active_model_serializers/blob/master/docs/howto/serialize_poro.md
+ActiveModelSerializers::Model.derive_attributes_from_names_and_fix_accessors
